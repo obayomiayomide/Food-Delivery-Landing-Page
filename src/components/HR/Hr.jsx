a72644e7e7812743d61b9f04ce06efca34aa63a7
obayomiayomide <obayomiayomide2020@gmail.com>
2024-08-29 19:08:17 +0100

    Desktop View

--- src/components/HR/Hr.jsx
+++ src/components/HR/Hr.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function Hr() {
   return (
-    <hr style={{ width: "60%", marginTop: "50px", marginBottom: "50px" }} />
+    <hr
+      style={{
+        width: "60%",
+        marginTop: "50px",
+        marginBottom: "50px",
+        color: "rgb(204, 204, 204)",
+      }}
+    />
   );
 }
 
